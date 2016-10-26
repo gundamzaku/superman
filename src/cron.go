0b75507250b6746a0879b95d8d8feb04fa40ae81
gundamzaku <gundamzaku@163.com>
2016-10-26 17:57:46 +0800

    go取linux进程

--- src/cron.go
+++ src/cron.go
@@ -26,7 +26,7 @@ func main() {
 			//判断类型
 			//执行PHP脚本
 			//cmd := exec.Command("php", "/data/www/wei/script/test.php")
-			cmd:= exec.Command("/bin/sh", "-c", `ps -ef | grep -v "grep" | grep "queue"`)
+			cmd:= exec.Command(`ps -ef | grep -v "grep" | grep "queue"`)
 			cmd.Stderr = os.Stdout
 			cmd.Stderr = os.Stderr
 			err := cmd.Run()
