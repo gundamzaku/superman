313e1fc11f005cc0ae5e6f05fd7b14d1bb87ea2e
gundamzaku <gundamzaku@163.com>
2016-10-26 17:45:06 +0800

    go取linux进程

--- src/cron.go
+++ src/cron.go
@@ -28,46 +28,11 @@ func main() {
 			//执行PHP脚本
 			//cmd := exec.Command("php", "/data/www/wei/script/test.php")
 			cmd:= exec.Command("/bin/sh", "-c", `ps -ef | grep -v "grep" | grep "queue"`)
-			stdout, err := cmd.StdoutPipe()
-			if err != nil {
-				fmt.Println("StdoutPipe: " + err.Error())
-				return
+			buf,err := cmd.Output()
+			if(err != nil){
+				fmt.Println(err)
 			}
-
-			stderr, err := cmd.StderrPipe()
-			if err != nil {
-				fmt.Println("StderrPipe: ", err.Error())
-				return
-			}
-
-			if err := cmd.Start(); err != nil {
-				fmt.Println("Start: ", err.Error())
-				return
-			}
-
-			bytesErr, err := ioutil.ReadAll(stderr)
-			if err != nil {
-				fmt.Println("ReadAll stderr: ", err.Error())
-				return
-			}
-
-			if len(bytesErr) != 0 {
-				fmt.Printf("stderr is not nil: %s", bytesErr)
-				return
-			}
-
-			bytes, err := ioutil.ReadAll(stdout)
-			if err != nil {
-				fmt.Println("ReadAll stdout: ", err.Error())
-				return
-			}
-
-			if err := cmd.Wait(); err != nil {
-				fmt.Println("Wait: ", err.Error())
-				return
-			}
-
-			fmt.Printf("stdout: %s", bytes)
+			fmt.Fprintf(os.Stdout, "Result: %s", buf)
 		}
 		time.Sleep(5 * time.Second)
 	}
