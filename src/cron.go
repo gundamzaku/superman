7d345a8a6eda1cc1e27a8482d486b3b0816f6628
gundamzaku <gundamzaku@163.com>
2016-11-03 11:17:39 +0800

    让SHELL脚本在后台运行

--- src/cron.go
+++ src/cron.go
@@ -116,7 +116,7 @@ func main() {
 				if runtime.GOOS != "windows" {
 					//继续执行下去
 					show(1, "exec: %s", v.Svs[i].CronPath + v.Svs[i].CronName)
-					runCmd := exec.Command(v.Svs[i].CronBash, v.Svs[i].CronPath + v.Svs[i].CronName+"&")
+					runCmd := exec.Command(v.Svs[i].CronBash, v.Svs[i].CronPath + v.Svs[i].CronName,"&")
 					runCmd.Stderr = os.Stdout
 					runCmd.Stderr = os.Stderr
 					buf, err := runCmd.Output()
