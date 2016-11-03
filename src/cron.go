078a9364b6de27d965596052f05dc81e46d15e06
gundamzaku <gundamzaku@163.com>
2016-11-03 16:03:03 +0800

    第一版，读取XML配置

--- src/cron.go
+++ src/cron.go
@@ -11,7 +11,6 @@ import (
 	"runtime"
 	"os/exec"
 	"strings"
-	"bytes"
 )
 
 type Recurlyservers struct {
@@ -124,7 +123,7 @@ func (cron *Cron) Run() {
 func (cron *Cron) RunProcess(i int){
 	//继续执行下去
 	cron.Show(1, "exec: %s %s", cron.view.Cron[i].CronPath + cron.view.Cron[i].CronName,cron.view.Cron[i].CronParam)
-	runCmd := exec.Command(cron.view.Cron[i].CronBash, cron.view.Cron[i].CronPath + cron.view.Cron[i].CronName,cron.view.Cron[i].CronParam,">>cron.txt &")
+	runCmd := exec.Command(cron.view.Cron[i].CronBash, cron.view.Cron[i].CronPath + cron.view.Cron[i].CronName,cron.view.Cron[i].CronParam,">>/tmp/cron.txt &")
 
 	runCmd.Stderr = os.Stdout
 	runCmd.Stderr = os.Stderr
