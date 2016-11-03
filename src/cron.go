5be151f4d4e3d0651089856de74ffe08536f92bc
gundamzaku <gundamzaku@163.com>
2016-11-03 15:45:07 +0800

    第一版，读取XML配置

--- src/cron.go
+++ src/cron.go
@@ -11,7 +11,7 @@ import (
 	"runtime"
 	"os/exec"
 	"strings"
-
+	"bytes"
 )
 
 type Recurlyservers struct {
@@ -124,12 +124,10 @@ func (cron *Cron) Run() {
 func (cron *Cron) RunProcess(i int){
 	//继续执行下去
 	cron.Show(1, "exec: %s %s", cron.view.Cron[i].CronPath + cron.view.Cron[i].CronName,cron.view.Cron[i].CronParam)
-	go exec.Command(cron.view.Cron[i].CronBash, cron.view.Cron[i].CronPath + cron.view.Cron[i].CronName,cron.view.Cron[i].CronParam)
-	/*
+	runCmd := exec.Command(cron.view.Cron[i].CronBash, cron.view.Cron[i].CronPath + cron.view.Cron[i].CronName,cron.view.Cron[i].CronParam)
 	var b bytes.Buffer
 	runCmd.Stdout = &b
-	runCmd.Start()	//这个是表示不阻塞的，还有阻塞的，用run()
-	*/
+	go runCmd.Start()	//这个是表示不阻塞的，还有阻塞的，用run()
 	/*
 	runCmd.Stderr = os.Stdout
 	runCmd.Stderr = os.Stderr
