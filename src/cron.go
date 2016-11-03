063e8a5ea8e119e6d2ed5c9832509a8ca8776fc0
gundamzaku <gundamzaku@163.com>
2016-11-03 14:11:28 +0800

    让SHELL脚本在后台运行

--- src/cron.go
+++ src/cron.go
@@ -1,22 +1,22 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"os"
 	"log"
-	"encoding/xml"
 	"io/ioutil"
 	"time"
 	"regexp"
+	"runtime"
 	"os/exec"
 	"strings"
-	"runtime"
 )
 
 type Recurlyservers struct {
 	XMLName     xml.Name `xml:"crons"`
 	Version     string   `xml:"version,attr"`
-	Svs         []cron `xml:"cron"`
+	Cron        []cron `xml:"cron"`
 	Description string   `xml:",innerxml"`
 }
 type cron struct {
@@ -29,18 +29,25 @@ type cron struct {
 	CronParam    string   `xml:"cronParam"`
 	CronInterval int64    `xml:"cronInterval"`
 }
-
 const TIMESLEEPINTERVAL = 3
-// 先声明map
-var mContainer map[string]int64
-var buf []byte = nil
+/*
+声明一个脚本类
+ */
+type Cron struct {
+	mContainer map[string]int64
+	buf []byte
+	view Recurlyservers
+}
 
-func main() {
-	//产生一个MAP
-	mContainer = make(map[string]int64)
-	//要改成读文件
+/*
+打开文件
+*/
+func (cron *Cron) OpenFile() {
+
+	//接受外部参数
 	args := os.Args
-	show(1, "read file who's name is %s", args[1])
+	cron.Show(1, "read file who's name is %s", args[1])
+
 	inputFile, inputError := os.Open(args[1])//变量指向os.Open打开的文件时生成的文件句柄
 	if inputError != nil {
 		log.Fatalf("Read File error:%s", inputError)
@@ -49,98 +56,20 @@ func main() {
 	if (err != nil) {
 		log.Fatalf("Read Xml error:%s", err)
 	}
-	v := Recurlyservers{}
-	xml.Unmarshal(xmlData, &v)
+	//解析XML
+	xml.Unmarshal(xmlData, &cron.view)
 	inputFile.Close()    //关闭文件
-	if len(v.Svs) == 0 {
+	if len(cron.view.Cron) == 0 {
 		log.Fatalf("No script need to run……")
 	}
-	for {
-		show(1,"\n\n\nloop_____________________________")
-		for i := 0; i < len(v.Svs); i++ {
-
-			//CronId必须存在
-			if v.Svs[i].CronId == "" {
-				show(3, "CronId didn't null……")
-				time.Sleep(TIMESLEEPINTERVAL * time.Second)
-				continue
-			}
-			//CronId必须为字母数字
-			reg, _ := regexp.Compile(`[^a-zA-Z0-9_]`)
-			if len(reg.FindAllString(v.Svs[i].CronId, -1)) > 0 {
-				show(3, "CronId must range [a-zA-Z0-9_]……")
-				time.Sleep(TIMESLEEPINTERVAL * time.Second)
-				continue
-			}
-
-			//存入MAP，记录时间间隔
-			if (mContainer[v.Svs[i].CronId] == 0) {
-				//初始化，执行时间
-				show(1, "init runtime")
-				mContainer[v.Svs[i].CronId] = time.Now().Unix()
-			}
-
-			if (mContainer[v.Svs[i].CronId] > time.Now().Unix()) {
-				//还未到执行时间
-				show(1, "runtime is not coming")
-				time.Sleep(TIMESLEEPINTERVAL * time.Second)
-				continue
-			} else {
-				//向后退移执行时间
-				mContainer[v.Svs[i].CronId] += v.Svs[i].CronInterval
-				show(2, "delay runtime to %d", mContainer[v.Svs[i].CronId])
-			}
-			show(1, "regular expression have done,pass")
-			var buf []byte = nil
-			if runtime.GOOS != "windows" {
-				cmd := exec.Command("/bin/sh", "-c", `ps -ef |grep -v "grep" |grep "` + v.Svs[i].CronName + ` `+v.Svs[i].CronParam+`"`)
-				cmd.Stderr = os.Stdout
-				cmd.Stderr = os.Stderr
-
-				buf, err = cmd.Output()
-				if (err != nil) {
-					show(4, "%s",err)
-				}
-				//show(1, "Result1: %s", buf)
-			}
-			//查找是否在进程中存在该程序
-			s := byteString(buf)
-			rs := strings.Contains(s, v.Svs[i].CronName)
-			if (rs == true) {
-				//此次不执行
-				show(2, "The process is running now")
-				time.Sleep(TIMESLEEPINTERVAL * time.Second)
-				continue
-			} else {
-				if runtime.GOOS != "windows" {
-					//继续执行下去
-					show(1, "exec: %s", v.Svs[i].CronPath + v.Svs[i].CronName)
-					runCmd := exec.Command(v.Svs[i].CronBash, v.Svs[i].CronPath + v.Svs[i].CronName,v.Svs[i].CronParam)
-					runCmd.Stderr = os.Stdout
-					runCmd.Stderr = os.Stderr
-					runCmd.Start()
-					buf, err := runCmd.Output()
-					if (err != nil) {
-						show(4,"%s",err)
-					}
-					fmt.Fprintf(os.Stdout, "Result: %s", buf)
-				}
-				time.Sleep(TIMESLEEPINTERVAL * time.Second)
-			}
-		}
-	}
+	//return cron.view
 }
 
-func byteString(p []byte) string {
-	for i := 0; i < len(p); i++ {
-		if p[i] == 0 {
-			return string(p[0:i])
-		}
-	}
-	return string(p)
-}
+/*
+打开文件
+*/
+func (cron *Cron) Show(status int, format string, a ...interface{}) {
 
-func show(status int, format string, a ...interface{}) {
 	if (status == 1) {
 		fmt.Print("[Info]    ")
 	} else if (status == 2) {
@@ -153,4 +82,127 @@ func show(status int, format string, a ...interface{}) {
 
 	fmt.Fprintf(os.Stdout, format, a...)
 	fmt.Println("")
+}
+func (cron *Cron)ByteString(p []byte) string {
+	for i := 0; i < len(p); i++ {
+		if p[i] == 0 {
+			return string(p[0:i])
+		}
+	}
+	return string(p)
+}
+
+//启动
+func (cron *Cron) Run() {
+
+	var err bool
+
+	for i := 0; i < len(cron.view.Cron); i++ {
+		err = cron.CheckCronId(i)
+		if(err == false){
+			continue
+		}
+		//分配时间间隔
+		err = cron.SetInterVal(i,cron.view.Cron[i].CronId)
+
+		if(err == false){
+			continue
+		}
+		cron.Show(1, "regular expression have done,pass")
+
+		if runtime.GOOS != "windows" {
+			//查看进程
+			err = cron.CheckProcess(i)
+			if(err == false){
+				continue
+			}
+			cron.RunProcess(i)
+		}
+	}
+}
+func (cron *Cron) RunProcess(i int){
+	//继续执行下去
+	cron.Show(1, "exec: %s", cron.view.Cron[i].CronPath + cron.view.Cron[i].CronName)
+	runCmd := exec.Command(cron.view.Cron[i].CronBash, cron.view.Cron[i].CronPath + cron.view.Cron[i].CronName,cron.view.Cron[i].CronParam)
+	runCmd.Stderr = os.Stdout
+	runCmd.Stderr = os.Stderr
+	runCmd.Start()	//这个是表示不阻塞的，还有阻塞的，用run()
+	buf, err := runCmd.Output()
+	if (err != nil) {
+		cron.Show(4,"%s",err)
+	}
+	cron.Show(1,"Result: %s", buf)
+	//fmt.Fprintf(os.Stdout, "Result: %s", buf)
+}
+
+func (cron *Cron) CheckProcess(i int) bool{
+
+	cmd := exec.Command("/bin/sh", "-c", `ps -ef |grep -v "grep" |grep "` + cron.view.Cron[i].CronName + ` `+cron.view.Cron[i].CronParam+`"`)
+	cmd.Stderr = os.Stdout
+	cmd.Stderr = os.Stderr
+	buf, err := cmd.Output()
+	if (err != nil) {
+		cron.Show(4, "%s",err)
+	}
+
+	s := cron.ByteString(buf)
+	rs := strings.Contains(s, cron.view.Cron[i].CronName)
+	if (rs == true) {
+		//此次不执行
+		cron.Show(2, "The process is running now")
+		time.Sleep(TIMESLEEPINTERVAL * time.Second)
+		return false
+	}
+	return true
+}
+
+func (cron *Cron) SetInterVal(i int,cronId string) bool {
+	//存入MAP，记录时间间隔
+	if (cron.mContainer[cronId] == 0) {
+		//初始化，执行时间
+		cron.Show(1, "init runtime")
+		cron.mContainer[cronId] = time.Now().Unix()
+	}
+
+	if (cron.mContainer[cronId] > time.Now().Unix()) {
+		//还未到执行时间
+		cron.Show(1, "runtime is not coming")
+		time.Sleep(TIMESLEEPINTERVAL * time.Second)
+		return false
+	} else {
+		//向后退移执行时间
+		cron.mContainer[cronId] += cron.view.Cron[i].CronInterval
+		cron.Show(2, "delay runtime to %d", cron.mContainer[cronId])
+	}
+	return true
+}
+
+//启动
+func (cron *Cron) CheckCronId(i int) bool {
+	//CronId必须存在
+	if cron.view.Cron[i].CronId == "" {
+		cron.Show(3, "CronId didn't null……")
+		time.Sleep(TIMESLEEPINTERVAL * time.Second)
+		return false
+	}
+	//CronId必须为字母数字
+	reg, _ := regexp.Compile(`[^a-zA-Z0-9_]`)
+	if len(reg.FindAllString(cron.view.Cron[i].CronId, -1)) > 0 {
+		cron.Show(3, "CronId must range [a-zA-Z0-9_]……")
+		time.Sleep(TIMESLEEPINTERVAL * time.Second)
+		return false
+	}
+	return true
+}
+
+func main()  {
+	//构造
+	cron := Cron{make(map[string]int64),nil,Recurlyservers{}}
+	cron.OpenFile()
+
+	for{
+		cron.Show(1,"loop %d",time.Now().Unix())
+		cron.Run()
+		time.Sleep(TIMESLEEPINTERVAL * time.Second)
+	}
 }
